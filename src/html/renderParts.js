9e43effa54940d0689063159a523b5aa4840745f
Jens Vetter <jensvetter@ymail.com>
2021-03-22 19:55:40 +0100

    minor css change

--- src/html/renderParts.js
+++ src/html/renderParts.js
@@ -25,17 +25,17 @@ export function renderParts(_parts) {
 
 
 	
-    let html = ''
-	// iterate through parts collection
-	Object.keys(_parts).forEach(part => {
-		html+= `
-		<input type="button" class="w3-button w3-round-large w3-border w3-black w3-medium" value="${part}"></input>   
-		`;
+    // let html = ''
+	// // iterate through parts collection
+	// Object.keys(_parts).forEach(part => {
+	// 	html+= `
+	// 	<input type="button" class="w3-button w3-round-large w3-border w3-black w3-medium" value="${part}"></input>   
+	// 	`;
 		
-	});
+	// });
 
-	document.getElementById(divID).innerHTML = '';
-	document.getElementById(divID).innerHTML += html;
+	// document.getElementById(divID).innerHTML = '';
+	// document.getElementById(divID).innerHTML += html;
 
 }
 ;
