2d1cc0edad22282c047b0adb5b3fa735a9e4946a
Jens Vetter <post@jensvetter.de>
2021-03-26 00:11:50 +0100

    re-installed socket npm libs.

--- client/index.js
+++ client/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "localhost" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54306" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55517" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
