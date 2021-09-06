324e9ab86ae714f1781b834b76d3e3af09905242
Jens Vetter <post@jensvetter.de>
2021-09-06 16:25:26 +0200

    start new step sequencer engine

--- src/js/app.js
+++ src/js/app.js
@@ -77,6 +77,7 @@ class WelleApp {
     map = (value, x1, y1, x2, y2) =>
         Math.round((((value - x1) * (y2 - x2)) / (y1 - x1) + x2) * 100) / 100;
 
+    // new step sequencer data
     //
     //
     //
